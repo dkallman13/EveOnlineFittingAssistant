f2e3b0622a0a83e0268819efa85f4b587b253039
Dan Kallman <dkallman13@gmail.com>
2021-10-07 15:01:21 -0400

    forgot the gitignore file

---
+++ EveOnlineFittingAssistant_Data/ActiveModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveOnlineFittingAssistant_Data
+{
+    public class ActiveModule : Module
+    {
+
+        public bool IsActiveModule = true;
+
+        //in Seconds
+        [Required]
+        public double CycleTime { get; set; }
+
+        public double CapacitorUsage { get; set; }
+
+        public ActiveModule(SlotType slot, double power, double cpu, double cycle) : base(slot, power, cpu)
+        {
+            CycleTime = cycle;
+
+        }
+        public ActiveModule(SlotType slot, double power, double cpu, double cycle, double capusage) : base(slot, power, cpu)
+        {
+            CycleTime = cycle;
+            CapacitorUsage = capusage;
+        }
+    }
+}
---
+++ EveOnlineFittingAssistant_Data/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveOnlineFittingAssistant_Data
+{
+    public class Weapon : ActiveModule
+    {
+        [Required]
+        public WeaponType TypeOfWeapon { get; set; }
+
+        [Required]
+        public double DamageMultiplier { get; set; }
+
+        public bool IsWeapon = true;
+
+        public Weapon(SlotType slot, double power, double cpu, double cycle, WeaponType type, double mult) :  base(slot, power, cpu, cycle)
+        {
+            TypeOfWeapon = type;
+            DamageMultiplier = mult;
+        }
+        public Weapon(SlotType slot, double power, double cpu, double cycle, double capusage, WeaponType type, double mult) : base(slot, power, cpu, cycle, capusage)
+        {
+            TypeOfWeapon = type;
+            DamageMultiplier = mult;
+        }
+    }
+    public enum WeaponType { Missile, Laser, Projectile, Hybrid, EntropicDesintigrator, VortronProjector}
+}
