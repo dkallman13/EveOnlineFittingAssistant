d3ad13175fb83ede8d82440b5185b6499808c387
Dan Kallman <dkallman13@gmail.com>
2021-10-11 10:36:09 -0400

    added ship data class

---
+++ EveOnlineFittingAssistant_Data/Fit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveOnlineFittingAssistant_Data
+{
+    public class Fit
+    {
+
+    }
+}
