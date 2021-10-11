3697bf0115d1dd7168408224cbd75859a02fb6f4
Dan Kallman <dkallman13@gmail.com>
2021-10-11 16:46:00 -0400

    added the data class for a fitted ship

--- EveOnlineFittingAssistant_Data/Fit.cs
+++ EveOnlineFittingAssistant_Data/Fit.cs
@@ -9,6 +9,17 @@ namespace EveOnlineFittingAssistant_Data
 {
     public class Fit
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ShipId { get; set; }
+        public List<int> HighModuleIds { get; set; }
+        public List<int> MidModuleIds { get; set; }
+        public List<int> LowModuleIds { get; set; }
+        public virtual Ship Ship { get; set; }
+        public virtual List<Module> HighModules { get; set; }
+        public virtual List<Module> MidModules { get; set; }
+        public virtual List<Module> LowModules { get; set; }
 
     }
 }
